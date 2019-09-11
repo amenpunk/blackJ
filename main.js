1694d38bc1abd918d52b3c9c49cdc41ac65d8741
amenpunk <ondasycircuitos@gmail.com>
2019-09-11 13:09:15 -0600

    ventana emergente :art:

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 var cuentaJugador = 0;
 
 window.onload = function () {
+
+       // Get the modal
+    var modal = document.getElementById("myModal")
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn")
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0]
+    // When the user clicks the button, open the modal 
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+     
+   
     var objCarta = document.getElementById('micarta');
     objCarta.onclick = function () {
         objCarta.className = 'carta carta1';
@@ -15,9 +35,13 @@ window.onload = function () {
     btnDarCarta.onclick = function () {
         var nuevaCarta = paquete.darCarta();
         var suma = mesaJugador.insertarCarta(nuevaCarta);
+        document.getElementById('ptn-uno').innerHTML = suma;
 
-        if (suma > 21)
-            document.getElementById('punteoJugador').innerHTML = 'PERDIO';
+        console.log(suma)
+        if (suma > 21){
+        modal.style.display = "block"
+        document.getElementById('punteoJugador').innerHTML = 'PERDIO';
+        }
     }
 
     btnDetener.onclick = function () {
@@ -27,6 +51,7 @@ window.onload = function () {
             if (suma > 21)
                 document.getElementById('punteoContrincante').innerHTML = 'PERDIO';
         } while (suma < 21);
+        document.getElementById('ptn-dos').innerHTML = suma;
 
     }
 }
